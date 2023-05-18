0cfdf6484b6ae5680bee786c8640ed195336f867
Jelte Dirks <j.dirks@outlook.com>
2023-05-19 01:56:37 +0200

    connectin and auth works, had the socket ports wrong...
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,33 @@
 use dotenv;
 use serde_json::json;
+use tungstenite::connect;
+use url::Url;
 use std::env;
 
-const AUTH_ADDRESS: &str = "wss://wirepas-dev.peercode.nl:8811";
-const REALTIME_ADDRESS: &str = "wss://wirepas-dev.peercode.nl:8813";
+const AUTH_ADDRESS: &str = "wss://wirepas-dev.peercode.nl:8813";
+const REALTIME_ADDRESS: &str = "wss://wirepas-dev.peercode.nl:8811";
 
 fn main() {
     // Load the credentials to give to the auth service.
     dotenv::from_filename("credentials.env").ok().unwrap();
 
     let auth_json = json!({
-        "data": { "user": "$USER", "password": "$PASSWORD" },
+        "data": { "username": "$USERNAME", "password": "$PASSWORD" },
         "type": 1,
         "version": 5
     });
 
     let auth_string = auth_json.to_string();
-    let auth_string = auth_string.replace("$USER", &env::var("user").unwrap());
     let auth_string = auth_string.replace("$PASSWORD", &env::var("password").unwrap());
+    let auth_string = auth_string.replace("$USERNAME", &env::var("username").unwrap());
 
     dbg!(&auth_string);
+
+    let (mut socket, _) = connect(Url::parse(AUTH_ADDRESS).unwrap()).unwrap();
+    socket.write_message(tungstenite::Message::Text(auth_string)).unwrap();
+
+    loop {
+        let msg = socket.read_message().expect("problem reading msg");
+        dbg!(msg);
+    }
 }
