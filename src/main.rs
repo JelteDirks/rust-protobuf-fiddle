45250e57da5c1e08e5d1d877649aa7853275c850
Jelte Dirks <j.dirks@outlook.com>
2023-05-19 01:36:34 +0200

    setting up the auth message
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 use dotenv;
-use websocket::client::builder::ClientBuilder;
+use serde_json::json;
 use std::env;
 
-const AUTH_ADDRESS: &str =  "wss://wirepas-dev.peercode.nl:8811";
+const AUTH_ADDRESS: &str = "wss://wirepas-dev.peercode.nl:8811";
 const REALTIME_ADDRESS: &str = "wss://wirepas-dev.peercode.nl:8813";
 
 fn main() {
+    // Load the credentials to give to the auth service.
     dotenv::from_filename("credentials.env").ok().unwrap();
 
+    let auth_json = json!({
+        "data": { "user": "$USER", "password": "$PASSWORD" },
+        "type": 1,
+        "version": 5
+    });
 
-    let client = ClientBuilder::new(AUTH_ADDRESS);
+    let auth_string = auth_json.to_string();
+    let auth_string = auth_string.replace("$USER", &env::var("user").unwrap());
+    let auth_string = auth_string.replace("$PASSWORD", &env::var("password").unwrap());
+
+    dbg!(&auth_string);
 }
